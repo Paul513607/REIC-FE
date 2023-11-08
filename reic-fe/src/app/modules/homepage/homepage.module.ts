553c868a3137f337868d7271559c44b9151ce716
Paul Samson <79131709+Paul513607@users.noreply.github.com>
2023-11-08 11:42:37 +0200

    changed location-settings to groups

--- reic-fe/src/app/modules/homepage/homepage.module.ts
+++ reic-fe/src/app/modules/homepage/homepage.module.ts
@@ -3,17 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { CalculatorSettingsComponent } from './calculator-settings/calculator-settings.component';
-import { LocationSettingsComponent } from './location-settings/location-settings.component';
 import { MapComponent } from './map/map.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { GroupsComponent } from './groups/groups.component';
 
 
 @NgModule({
   declarations: [
     HomepageComponent,
     CalculatorSettingsComponent,
-    LocationSettingsComponent,
     MapComponent,
+    GroupsComponent,
   ],
   imports: [
     CommonModule,
